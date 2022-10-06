712cf8d408e59ad9a0a71e05eb34d6a1135a8605
Anas-Awni <anoosalkhabbas@gmail.com>
2022-10-06 19:30:01 +0300

    go to book page

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,3 +1,4 @@
+import 'package:elearning/screens/pdf.dart';
 import 'package:elearning/screens/quizz.dart';
 import 'package:flutter/material.dart';
 
@@ -82,7 +83,12 @@ class _HomeState extends State<Home> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 GestureDetector(
-                  onTap: () {},
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return PdfBook();
+                    }));
+                  },
                   child: Container(
                     alignment: Alignment.center,
                     height: 140,
