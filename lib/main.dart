03671e6def5094331af032fe56d77ff74d37487a
mohammad6202 <m.almsedin@gmail.com>
2022-10-06 11:33:09 +0300

    Elearning

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'Home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
